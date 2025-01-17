60772ae004df845230a8295acc4853d72b6da9ea
Arthur Ferrão <arthur.ferrao@vtex.com>
2025-01-17 11:58:25 -0300

    feat: encode shipping info to base64

--- react/client.ts
+++ react/client.ts
@@ -1,8 +1,6 @@
 import { SHIPPING_INFO_COOKIE } from './constants'
 import { setCookie } from './utils/cookie'
 
-export const ALL_SEMICOLON_REGEX = /;/g
-
 export const getAddress = (
   countryCode: string,
   zipCode: string,
@@ -22,8 +20,8 @@ export const updateSession = (
   )}${pickup ? `;pickupPoint=${pickup.pickupPoint.id}` : ''}`
 
   // __RUNTIME__.segmentToken is not reliable for the facets. It might not be updated. For this reason we must try to get the info from our custom cookie first
-  // Replacing ";" by ":" because ";" is not allowed in cookies
-  setCookie(SHIPPING_INFO_COOKIE, facetsValue.replace(ALL_SEMICOLON_REGEX, ':'))
+  // Encode to base64 because ";" is not allowed in cookies
+  setCookie(SHIPPING_INFO_COOKIE, btoa(facetsValue))
 
   return fetch('/api/sessions', {
     method: 'POST',
